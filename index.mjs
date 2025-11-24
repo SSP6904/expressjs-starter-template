2b647371dc7ef3fc71f43c4ec5ddbdcf9e0d6800
Shaun Hoffer <75172901+SSP6904@users.noreply.github.com>
2025-11-23 20:28:01 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,33 +1,32 @@
 // Packages go here
 import express from 'express'
 
-// Variables go here
-const app = express()
-const port = process.env.PORT || 8000
-
-// Server options go here
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-
-// Serer routes and handlers go here
-app.get('/', function(req, res) {
-    res.send(`This is the root page! Nice to see you!`)
-})
-app.get('/about', function(req, res) {
-    res.send(`This is the about page! Nice to see you!`)
-})
-app.use(function(req, res) {
-    return res.status(404).json({status: 404, message: "That page isn't on this server!"})
-})
-
-// Listening route goes here
-app.listen(port, function() {
-    try {
-        console.log(`Started the express server! Click on the link below to see it in your browser!`)
-        console.log(`http://localhost:${port}`)
-    } catch(error) {
-        const message = error instanceof Error ? error.message : 'An unknown error has occurred!'
-        console.error(message)
+(async function() {
+    // Variables go here
+    const app = express()
+    app.config = {
+        port: process.env.PORT || 8000
     }
-})
+
+    // Server options go here
+    app.use(express.static('public'))
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+
+    // Serer routes and handlers go here
+    app.get('/', function(req, res) {
+        res.send(`This is the root page! Nice to see you!`)
+    })
+    app.get('/about', function(req, res) {
+        res.send(`This is the about page! Nice to see you!`)
+    })
+    app.use(function(req, res) {
+        return res.status(404).json({status: 404, message: "That page isn't on this server!"})
+    })
+
+    // Listening route goes here
+    app.listen(app.config.port, function() {
+        console.log(`Started the express server!`)
+        console.log(`Forwarded URL: http://localhost:${app.config.port}`)
+    })
+})()
